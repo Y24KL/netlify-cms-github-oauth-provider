a723fc349574c5151dd2dd57961fb22287843da2
Y24KL <gtrusouth116@gmail.com>
2026-04-03 00:57:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
-require('dotenv').config({ silent: true })
-const express = require('express')
-const middleWarez = require('./index.js')
-const port = process.env.PORT || 3000
+const express = require('express');
+const { AuthorizationCode } = require('simple-oauth2');
+const randomstring = require('randomstring');
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Initial page redirecting to Github
-app.get('/auth', middleWarez.auth)
+const oauth2 = new AuthorizationCode({
+  client: {
+    id: process.env.OAUTH_CLIENT_ID,
+    secret: process.env.OAUTH_CLIENT_SECRET,
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  },
+});
 
-// Callback service parsing the authorization token
-// and asking for the access token
-app.get('/callback', middleWarez.callback)
+app.get('/auth', (req, res) => {
+  const authorizationUri = oauth2.authorizeURL({
+    redirect_uri: `${process.env.REDIRECT_URL}/callback`, // This builds the link GitHub needs
+    scope: 'repo,user',
+    state: randomstring.generate(),
+  });
+  res.redirect(authorizationUri);
+});
 
-app.get('/success', middleWarez.success)
-app.get('/', middleWarez.index)
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+  const options = {
+    code,
+    redirect_uri: `${process.env.REDIRECT_URL}/callback`,
+  };
+
+  try {
+    const accessToken = await oauth2.getToken(options);
+    const token = accessToken.token.access_token;
+
+    res.send(`
+      <html><body><script>
+        (function() {
+          function receiveMessage(e) {
+            window.opener.postMessage('authorization:github:success:{"token":"${token}","provider":"github"}', e.origin);
+          }
+          window.addEventListener("message", receiveMessage, false);
+          window.opener.postMessage("authorizing:github", "*");
+        })()
+      </script></body></html>
+    `);
+  } catch (error) {
+    res.status(500).json('Authentication failed');
+  }
+});
 
 app.listen(port, () => {
-  console.log("Netlify CMS OAuth provider listening on port " + port)
-})
+  console.log(`Proxy listening on port ${port}`);
+});
